f6516599824350bf200e4aa0b0f0ab473433f416
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2024-10-15 20:01:30 +0200

    Correct sh file

--- ubuntu_hyprland.sh
+++ ubuntu_hyprland.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo apt-get update && sudo apt-get upgrade
+sudo apt install node-typescript npm meson libgjs-dev gjs libgtk-layer-shell-dev libgtk-3-dev libpulse-dev network-manager-dev libgnome-bluetooth-3.0-dev libdbusmenu-gtk3-dev libsoup-3.0-dev
+sudo apt-get install rofi
 git clone -b 24.04 --depth=1 https://github.com/JaKooLit/Ubuntu-Hyprland.git ~/Ubuntu-Hyprland
 cd ~/Ubuntu-Hyprland
 chmod +x install.sh
